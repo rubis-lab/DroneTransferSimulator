1915b229adbf30a55b17a56f74fe2b567a96b127
hexoul <crosien@gmail.com>
2017-07-21 18:18:40 +0900

    Add basic functions in Simulator

--- Simulator.cpp
+++ Simulator.cpp
@@ -1,2 +1,35 @@
+/**
+@file
+@date 2017/07/21
+@author Minji and hexoul
+@brief
+*/
 #include "Simulator.h"
+#include <algorithm>
 
+std::vector<Event> Simulator::getEvents()
+{
+	return events;
+}
+
+/**
+@brief find the range of event from start time to end time
+@details
+@param starttime, endtime, event vector
+@return list of number of events
+*/
+void Simulator::updateEventsBtwRange(time_t start, time_t end)
+{
+	std::sort(events.begin(), events.end());
+}
+
+/**
+@brief simulating
+@details simulate each events
+@param event
+@return time
+*/
+void Simulator::simulation()
+{
+
+}
--- Simulator.h
+++ Simulator.h
@@ -1,12 +1,19 @@
+#include <vector>
+#include <ctime>
+#include "Event.h"
+
 #ifndef _H_SIMULATOR_
 #define _H_SIMULATOR_
 
 class Simulator
 {
 private:
+	std::vector<Event> events;
 
 public:
-
+	std::vector<Event> getEvents();
+	void updateEventsBtwRange(time_t start, time_t end);
+	void simulation();
 };
 
 #endif
