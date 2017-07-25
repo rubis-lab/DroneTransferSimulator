8cf4a186d2216119b50abce56302cb9e16ba31ea
hexoul <crosien@gmail.com>
2017-07-25 17:59:45 +0900

    Replace the definition of cube size. Define upper and lower bound of Seoul_40x40_500x500 table.

--- PathPlanner.h
+++ PathPlanner.h
@@ -1,9 +1,16 @@
 #include <vector>
 
-#define CUBE_SIZE	10
-
 #ifndef _H_PATH_PLANNER_
 #define _H_PATH_PLANNER_
+
+#define CUBE_SIZE	10
+
+#define UB_LAT		37.680000	/* Upper bound of latitute for Seoul_40x40_500x500 table */
+#define UB_LNG		126.783400	/* Upper bound of longitude for Seoul_40x40_500x500 table */
+#define LB_LAT		37.432499	/* Lower bound of latitute for Seoul_40x40_500x500 table */
+#define LB_LNG		127.197999	/* Lower bound of longitude for Seoul_40x40_500x500 table */
+#define D_LAT		((UL_LAT - LR_LAT) / 20000)
+#define D_LNG		((LR_LNG - UL_LNG) / 20000)
 
 class PathPlanner
 {
