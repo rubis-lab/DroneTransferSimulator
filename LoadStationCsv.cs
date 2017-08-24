461da5f5989f54afab81f441dd58e86d2c50542c
DESKTOP-489IJBP\이민지 <mingg9688@gmail.com>
2017-08-24 13:17:27 +0900

    Upload winform files

---
+++ LoadStationCsv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DroneTransferSimulator
+{
+    public partial class LoadStationCsv : Form
+    {
+        simulator form1;
+        public LoadStationCsv()
+        {
+            InitializeComponent();
+        }
+
+        public LoadStationCsv(simulator _form)
+        {
+            InitializeComponent();
+            form1 = _form;
+        }
+    }
+}
