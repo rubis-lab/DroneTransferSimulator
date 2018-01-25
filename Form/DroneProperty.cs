a4db9823bf7063e77e92d51b9f6bbdef98bec76d
DESKTOP-LPT8JP6\lucetre <lucetre@naver.com>
2018-01-25 10:20:10 +0900

    Add drone property window

---
+++ Form/DroneProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DroneTransferSimulator
+{
+    public partial class DroneProperty : Form
+    {
+        public DroneProperty()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
